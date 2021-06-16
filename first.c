04e303b679ce2ce9731fe1c2baa279223d578475
AmanThakur01 <amanmrthakur@gmail.com>
2021-06-16 11:11:08 +0530

    commit1

---
+++ first.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+// struct first
+// {
+//     int id;
+// };
+
+void main()
+{
+    // int i =0;
+    // if (i=5)
+    //     printf("%d\n",i);
+    // else
+    //     printf("else");
+
+    // struct first p1;
+    // p1.id=11;
+    // printf("%d\n",p1.id);
+    ////////////////////////////////
+    // int a, b;
+    // a = (-1, 0, 1);
+    // b = a++;
+    // printf(" %d & %d\n", a, b);
+    /////////////////////////////////
+    //     int a = 0, b = 0;
+    //     printf("%d\n", a = a + 1);
+    //     printf("%d\n", a);
+
+
+
+ int x = 1; while(x = 0) printf("hello");
+}
